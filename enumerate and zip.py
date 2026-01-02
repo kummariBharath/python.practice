4662e6e7e4387c8cfd97c8b62692d094458c5dfb
kummariBharath <bharathkummari2611@gmail.com>
2026-01-02 19:54:50 +0530

    code

--- enumerate and zip.py
+++ enumerate and zip.py
@@ -3,3 +3,8 @@ infacturators=["Alice", "Bob", "Charlie"]
 for index,name in enumerate(infacturators, start=1):
     print(f"{index}: {name}")
 
+ #zip function is used to combine two or more iterables (like lists or tuples) into a single iterable of tuples   
+languages=["Python","Java","C++"] 
+levels=["Beginner","Intermediate","Advanced"]
+for lang,level in zip(languages,levels):
+    print(f"{lang} is for {level} level")
