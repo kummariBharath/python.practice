b4ddbd25bc12d11ea263889bba759b4bd20a7f0c
kummariBharath <bharathkummari2611@gmail.com>
2026-01-02 19:51:21 +0530

    code

--- enumerate and zip.py
+++ enumerate and zip.py
@@ -1,3 +1,5 @@
+#enumerate function is used to add a counter to an iterable and returns it as an enumerate object
 infacturators=["Alice", "Bob", "Charlie"]
 for index,name in enumerate(infacturators, start=1):
     print(f"{index}: {name}")
+
