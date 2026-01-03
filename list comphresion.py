baf1ecc8cd3354d036399d4eceb7c25006a00855
kummariBharath <bharathkummari2611@gmail.com>
2026-01-03 20:12:48 +0530

    code

--- list comphresion.py
+++ list comphresion.py
@@ -3,3 +3,5 @@ even_numbers=[x for x in range(1,88) if x%3==0]
 print(even_numbers)
 #creating tuples using list comprehension
 numbers=input("enter numbers sepparted by comma:")
+result=[(x,int(x)**2,int(x)**3) for x in numbers.split(",")]
+print(result)
