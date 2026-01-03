9363981c369c51c2d9c7476d673b00a4933ecff3
kummariBharath <bharathkummari2611@gmail.com>
2026-01-03 21:59:43 +0530

    code

--- list comphresion.py
+++ list comphresion.py
@@ -11,3 +11,8 @@ print(squares)
 #creating a set using list comprehension
 odd_numbers=[x for x in range(1,20) if x%2!=0]
 print(odd_numbers)
+
+#nested list comprehension
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+transposed=[[row[i] for row in matrix] for i in range(3)]
+print(transposed)
