a343e065d1ac07fdaeff275f5ac2f7bb855f8dcb
kummariBharath <bharathkummari2611@gmail.com>
2026-01-03 20:17:24 +0530

    code

--- list comphresion.py
+++ list comphresion.py
@@ -3,7 +3,7 @@ even_numbers=[x for x in range(1,88) if x%3==0]
 print(even_numbers)
 #creating tuples using list comprehension
 numbers=input("enter numbers sepparted by comma:")
-result=[(x,x**2,x**3) for x in numbers.split(",")]
+result=[(x,int(x)**2,int(x)**3) for x in numbers.split(",")]
 print(result)
 #creating a dictionary using list comprehension
 squares={x:x**2 for x in range(1,10)}
