8840872a75bb0f9e89c4e4b0bbf7201f44c08a21
kummariBharath <bharathkummari2611@gmail.com>
2026-01-03 20:10:15 +0530

    code

--- list comphresion.py
+++ list comphresion.py
@@ -1,3 +1,5 @@
 #list comprehensions
 even_numbers=[x for x in range(1,88) if x%3==0]
 print(even_numbers)
+#creating tuples using list comprehension
+numbers=inpu("enter numbers sepparted by comma:")
