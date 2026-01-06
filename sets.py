d7cb3a10817cc3c614e47f52cc2e5b5d441ecdff
kummariBharath <bharathkummari2611@gmail.com>
2026-01-06 16:20:48 +0530

    code

--- sets.py
+++ sets.py
@@ -4,3 +4,9 @@ my_set.add(66) #add()
 my_set.remove(3) #remove() to delete a element
 my_set.discard(1) #dicard() to delete a element
 print(my_set)
+
+#.issubset() and issuperset()
+my_set1={10,20,30,40,50}
+my_set2={20,30,40}
+print(my_set2.issubset(my_set1))
+print(my_set1.issuperset(my_set2))
