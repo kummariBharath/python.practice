0d668de7a8e2d7ecdad8da8d39d782d71ae7254b
kummariBharath <bharathkummari2611@gmail.com>
2026-01-06 15:37:47 +0530

    code

--- enumerate and zip.py
+++ enumerate and zip.py
@@ -14,3 +14,8 @@ scores=[85,92,78]
 rankings=[1,2,3] 
 for student,score,rank in zip(students,scores,rankings):
     print(f"{student} scored {score} and is ranked with {rank}")  
+
+#looping through a dictionary
+products={'laptop':100000,'phone':20000,'tablet':15000,'headphones':5000}
+for product,price in products.items():
+    print(f'The price of {product} is {price}')
