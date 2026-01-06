a90d7acacc5c62e5385318901e76aa6ace33c909
kummariBharath <bharathkummari2611@gmail.com>
2026-01-06 16:15:07 +0530

    code

--- sets.py
+++ sets.py
@@ -1,3 +1,6 @@
 #Sets are one of Python's built-in data structures. One of the core characteristics of sets is that they don't store duplicate values. 
 my_set={1,2,3,4,5}
-my_set()
+my_set.add(66) #add() 
+my_set.remove(3) #remove() to delete a element
+my_set.discard(1) #dicard() to delete a element
+print(my_set)
