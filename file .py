20b30195d8580b29c0b55c2a2b82ce5d255d1ea5
kummariBharath <bharathkummari2611@gmail.com>
2026-01-12 13:54:37 +0530

    code

--- file .py
+++ file .py
@@ -1,11 +1,19 @@
+#with open("bharath.txt","r") as f:
+ #   data=f.read()
+
+#new_data=data.replace("python","java") #replacing python with java
+#print(new_data)
+
+#with open("bharath.txt","w") as f:
+#    f.write(new_data)
+
+#finding the word "learning"
+word="learning"
 with open("bharath.txt","r") as f:
     data=f.read()
-
-new_data=data.replace("python","java") #replacing python with java
-print(new_data)
-
-with open("bharath.txt","w") as f:
-    f.write(new_data)
-
+    if(data.find(word)):
+        print("found")
+    else:
+        print("not found")    
 
     
