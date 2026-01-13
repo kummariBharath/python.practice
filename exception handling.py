21d293af4eb7d4f7844c341cdf5ed9b9aabdbf16
kummariBharath <bharathkummari2611@gmail.com>
2026-01-13 16:37:03 +0530

    code

--- exception handling.py
+++ exception handling.py
@@ -3,4 +3,5 @@ try:
     x=10/0
 except ZeroDivisionError:
     print("you can't divide by zero!")
-        
+#try is block of code where error might come
+# handles the  error raised from try block        
