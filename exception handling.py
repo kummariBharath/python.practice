c22e861efa932c1a27d2005450f8cd943cd1f882
kummariBharath <bharathkummari2611@gmail.com>
2026-01-13 17:16:34 +0530

    raise

--- exception handling.py
+++ exception handling.py
@@ -20,3 +20,34 @@ try:
     result=10/num  
 except (ValueError,ZeroDivisionError) as e:
     print(f"error :{e}")
+
+#Raise statement used to explicitly throe an exception at any point
+def process_data(data):
+    try:
+        result=int(data)
+        return result*2
+    except ValueError:
+        print('logging:invalid data received')
+        raise
+
+print(process_data(88))
+
+def file_config(filename):
+    try:
+        with open(filename,'r') as file:
+            data=file.read()
+            return int(data)
+    except FileNotFoundError:
+        raise ValueError('configuration file is missing')
+    except ValueError as e:
+        raise ValueError('invalid format') 
+print(file_config('bharath.txt'))   
+
+#asseration error
+def calculate_square_root(number):
+    assert number >=0
+    return number**0.5
+try:
+    result=calculate_square_root(-5)
+except AssertionError as e:
+    print(f"Asseration failed:{e}")    
