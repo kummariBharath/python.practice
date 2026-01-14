68e350827b01034606c57cdb7a4cebd65da0f13b
kummariBharath <bharathkummmari2611@gmail.com>
2026-01-14 17:27:33 +0530

    changes

--- class and objects.py
+++ class and objects.py
@@ -1,6 +1,14 @@
 #class is blueprint that is used to create the classes using class keyword
 class Bharath:
-    def __init__(self, name, age):
+    def __init__(self, name, days):
         self.name = name
-        self.age = age
-    
+        self.age = days
+    def learn(self):
+        print(f"{self.name.upper()} and learns python in {self.days}")
+#objects creation and calling from classes
+skill_1=Bharath("learns sql",30)
+skill_2=learn("learns DSA",25)   
+
+#calling the methods
+skill_1.learn()
+skill_2.learn()
