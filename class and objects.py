824b40a79c7bec0481e793d6dba970565f66bb80
kummariBharath <bharathkummmari2611@gmail.com>
2026-01-14 17:21:02 +0530

    class created

---
+++ class and objects.py
@@ -0,0 +1,6 @@
+#class is blueprint that is used to create the classes using class keyword
+class Bharath:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
