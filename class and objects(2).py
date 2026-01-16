054048cc8368eaac69db4a137bfaf479bc6a2e49
kummariBharath <bharathkummmari2611@gmail.com>
2026-01-16 15:02:48 +0530

    Enhance Cart class with additional methods for item management

--- class and objects(2).py
+++ class and objects(2).py
@@ -1,6 +1,6 @@
 class Cart:
    def __init__(self):
-       self.items = {}
+       self.items = {} #dictionary to store items
 
    def add(self, item):
        self.items[item] = True
@@ -12,5 +12,17 @@ class Cart:
    def display(self):
         for item in self.items:
              print(item)  
+   def __len__(self):
+        return len(self.items)
+   
+   def __contains__(self,item):
+        return item in self.items
+         
+   def insert(self,key,value):
+        self.items[key]=value
+
+   def  delete(self,key):
+        if key in self.items:
+             del self.items[key]    
 
                      
