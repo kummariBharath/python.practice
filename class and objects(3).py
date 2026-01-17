ab116c92bbccba566623a0e54625c4f4bff1ad32
kummariBharath <bharathkummari2611@gmail.com>
2026-01-17 12:17:25 +0530

    Add example of using dir() function to list object attributes

--- class and objects(3).py
+++ class and objects(3).py
@@ -7,3 +7,22 @@ book_1=book("python","Bharath")
 book_2=book("java","Alex") 
 print(getattr(book_1,"price","not avaliable")) #prints the default value if attribute not found
 print(getattr(book_2,"author","not avaliable")) #prints the attribute value if found
+
+#using the dir() function to list all attributes of an object
+class Person: 
+    def __init__(self, name, age): 
+        self.name = name 
+        self.age = age 
+
+person = Person('John Doe', 30)
+
+# Loop through all attributes of the person object with dir() function
+for attr in dir(person):
+    # Ignore dunder methods like __init__ or __str__ and regular methods
+    if not attr.startswith('__') and not callable(getattr(person, attr)):  #callable checks if the attribute is a method and ignores it
+        value = getattr(person, attr)
+        print(f'{attr}: {value}')
+
+# Output
+# age: 30
+# name: John Doe
