2903205285cc8e84c80d3b25d87f2b22c87a1847
kummariBharath <bharathkummari2611@gmail.com>
2026-01-18 23:43:33 +0530

    code change

--- xyz.py
+++ xyz.py
@@ -8,3 +8,23 @@ class Dog:
         self.age = age
 dog_1=Dog("buddy",3)
 print(dog_1.name)
+
+count = 4
+while (count < 5):    
+    count = count+1
+    print(count)
+    break
+else:
+    print("No Break")
+
+sum=0
+num=int(input("enter a number(0 to stop):"))
+while num !=0:
+    sum+=num
+    num=int(input("enter a number(0 to stop):"))
+print("The sum is:",sum) #output:enter a number(0 to stop):55
+                                 #enter a number(0 to stop):6168
+                                 #enter a number(0 to stop):28088888989899
+                                #enter a number(0 to stop):0
+                                #The sum is: 28088888996122
+
