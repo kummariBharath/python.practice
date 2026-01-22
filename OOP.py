ac0103dc3007a129ff605d21d4284bab7740d192
kummariBharath <bharathkummari2611@gmail.com>
2026-01-22 11:46:34 +0530

    new class

--- OOP.py
+++ OOP.py
@@ -1,16 +1,23 @@
 class Wallet:
     def __init__(self,balance):
-         self._balance=balance 
+         self.__balance=balance 
     def deposit(self,amount):
          if amount>0:
-              self._balance+=amount
+              self.__balance+=amount
     def withdraw(self,amount):
-         if amount<=self._balance:
+         if amount<=self.__balance:
               raise ValueError("insuffient funds")
     def get_balance(self):
-         return self._balance     
+         return self.__balance     
 acc_1=Wallet(500)
-print(acc_1.self._balance)    # Accessing the balance directly (not recommended)          
+print(acc_1.get_balance())    # with double underscore balance is private and accesible with methods only        
 
-         
+#deining a private method which does the internal work
+class calcy:
+    def add(self,a,b):
+         return self.__add_internal(a,b)
+    def __add_internal(self,a,b):
+         return a+b
+cal_1=calcy()
+print(cal_1.add(5,200))         
          
