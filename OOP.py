a5383134d166af29e6bc0b063aef4048b2ae34ba
kummariBharath <bharathkummari2611@gmail.com>
2026-01-22 11:39:00 +0530

    code

--- OOP.py
+++ OOP.py
@@ -1,3 +1,14 @@
 class Wallet:
     def __init__(self,balance):
-         self._balance=balance
+         self._balance=balance 
+    def deposit(self,amount):
+         if amount>0:
+              self._balance+=amount
+    def withdraw(self,amount):
+         if amount<=self._balance:
+              raise ValueError("insuffient funds")
+acc_1=Wallet(500)
+print(acc_1.self._balance)              
+
+         
+         
