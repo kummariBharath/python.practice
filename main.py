d9e3e6f3ad33bc6cbbe3856dc7d920a09091bf8e
kummariBharath <bharathkummari2611@gmail.com>
2026-01-26 11:51:52 +0530

    setter method

---
+++ main.py
@@ -0,0 +1,23 @@
+class GameCharacter:
+    def __init__(self,name=""):
+        self._name=name
+        self._health=100
+        self._mana=50
+        self._level=1
+    @property    
+    def name(self):
+        return self._name 
+    @property       
+    def health(self):
+        return self._health
+    @health.setter
+    def health(self,health):
+        if health<0:
+            self._health=0
+        if health>100:
+            self._health=100
+        if 0<=health<=100:
+            self._health=health
+        
+    
+    
