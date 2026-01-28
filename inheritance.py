f98c281f01fcca07ab243623958a6818ddb54899
kummariBharath <bharathkummari2611@gmail.com>
2026-01-28 21:47:47 +0530

    .

--- inheritance.py
+++ inheritance.py
@@ -6,4 +6,9 @@ class Developer:
         return f"{self.name_1} writes the logic for project"
     def test(self):
         return f"{self.name_2} tests the code"
+class Manager(Developer):
+    def __init__(self,name_1,name_2,name_3):
+        super().__init__(name_1,name_2) #super() is used to call the parent class constructor
+        self.name_3=name_3    
+    
     
