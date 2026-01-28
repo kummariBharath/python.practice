1e300bf700cd00f4df48ef08c9d5cbbac7cac741
kummariBharath <bharathkummari2611@gmail.com>
2026-01-28 22:33:52 +0530

    practice with creating class

---
+++ polymorphism.py
@@ -0,0 +1,13 @@
+class Company:
+    def __init__(self,name):
+        self.name=name
+    def hire(self):
+        return f"{self.name} started to hire freshers"
+class organization:
+    def hire(self):
+        return "organization started hiring"
+class industry:
+    def hire(self):
+        return "industry started to hire"    
+        
+    
