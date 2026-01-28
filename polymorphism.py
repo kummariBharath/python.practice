43fc71db5da7c1ab4430e10bd0ca505e37a32c85
kummariBharath <bharathkummari2611@gmail.com>
2026-01-29 00:00:13 +0530

    added the coments

--- polymorphism.py
+++ polymorphism.py
@@ -32,10 +32,26 @@ job_search(Company("TCS"))
 class parent:
     def __init__(self):
         self.__name="bharath" #private variable
-        def get_name(self):
-            return self.__name
+    def get_name(self):
+        return self.__name
 class child(parent):
-    def diplay(self):
-        return self.get_name()    
+    def display(self):
+        return self._parent__name  ###Inside class parent, you defined self.__name. Python stored this as self._parent__name.
+                                      # Inside class child, you wanted to access that specific variable.
+                                      # Since child inherits from parent, it has access to the data, but only under the "mangled" name.
+                                       #Therefore, you explicitly wrote self._parent__name to retrieve it.
+                                       #_parent__name is how to access private variable in child class
 c=child()
-print(c.diplay())
+print(c.display())
+
+
+class Example:
+    def __init__(self, internal, private):
+        self._internal = internal
+        self.__private = private
+
+example1 = Example(
+    'I can be accessed from outside the class, but should not',
+    'I cannot be accessed directly from outside the class'
+)
+print(example1.__dict__) #{'_Example__private': 'I cannot be accessed directly from outside the class'}
