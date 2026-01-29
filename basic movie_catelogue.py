c376732201fe091a4f31840dd283cab9614602f3
kummariBharath <bharathkummari2611@gmail.com>
2026-01-29 23:55:10 +0530

    completed with neccessary changes

--- basic movie_catelogue.py
+++ basic movie_catelogue.py
@@ -1,10 +1,9 @@
 class MediaError(Exception):
     """Custom exception for media-related errors."""
-    def __init__(self,message,obj):
-        super().__init__(message) #it inherits  from Exception class
-        self.obj=obj
 
-
+    def __init__(self, message, obj):
+        super().__init__(message)
+        self.obj = obj
 
 class Movie:
     """Parent class representing a movie."""
@@ -54,14 +53,29 @@ class MediaCatalogue:
             raise MediaError('Only Movie or TVSeries instances can be added', media_item)
         self.items.append(media_item)
 
+    def get_movies(self):
+        return [item for item in self.items if type(item) is Movie]
+
+    def get_tv_series(self):
+        return [item for item in self.items if isinstance(item, TVSeries)]
+    
     def __str__(self):
         if not self.items:
             return 'Media Catalogue (empty)'
+        
+        movies = self.get_movies()
+        series = self.get_tv_series()
 
         result = f'Media Catalogue ({len(self.items)} items):\n\n'
+        if movies:
+            result += '=== MOVIES ===\n'
+            for i, movie in enumerate(movies, 1):
+                result += f'{i}. {movie}\n'
+        if series:
+            result += '=== TV SERIES ===\n'
+            for i,tv_series in enumerate(series,1):
+                result +=f"{i}. {tv_series}\n"
         
-        for i, movie in enumerate(self.items, 1):
-            result += f'{i}. {movie}\n'
         return result
 
 catalogue = MediaCatalogue()
@@ -80,3 +94,6 @@ try:
     print(catalogue)
 except ValueError as e:
     print(f'Validation Error: {e}')
+except MediaError as e:
+    print(f'Media Error: {e}')
+    print(f'Unable to add {e.obj}: {type(e.obj)}')
