6da25fe5bd667e69ab8d755bfbcf18c6e422685a
kummariBharath <bharathkummari2611@gmail.com>
2026-01-29 22:47:59 +0530

    added methods

--- basic movie_catelogue.py
+++ basic movie_catelogue.py
@@ -8,22 +8,45 @@ class Movie:
         self.duration = duration
     def __str__(self):
         return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
-class MovieCatelogue:
+class TVSeries(Movie):
+    def __init__(self, title, year, director, duration, seasons, total_episodes):
+        super().__init__(title, year, director, duration)
+
+        if seasons < 1:
+            raise ValueError('Seasons must be 1 or greater')
+        if total_episodes < 1:
+            raise ValueError('Total episodes must be 1 or greater')
+        
+        self.seasons = seasons
+        self.total_episodes = total_episodes
+    def __str__(self):
+        return f"{self.title} ({self.year}) - {self.seasons} seasons, {self.total_episodes}  episodes, {self.duration} min avg, {self.director}"
+class MediaCatalogue:
     def __init__(self):
-        self.items=[]
+        self.items = []
+
     def add(self, media_item):
         self.items.append(media_item)
+
     def __str__(self):
         if not self.items:
-            return "Media Catalogue (empty)"
+            return 'Media Catalogue (empty)'
+
         result = f'Media Catalogue ({len(self.items)} items):\n\n'
-        for i, item in enumerate(self.items, start=1):
-            result += f'{i}. {item}\n'
+        
+        for i, movie in enumerate(self.items, 1):
+            result += f'{i}. {movie}\n'
         return result
-catalogue = MovieCatelogue()
+
+catalogue = MediaCatalogue()
+
+
 try:
     movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
     catalogue.add(movie1)
+    movie2 = Movie('Inception', 2010, 'Christopher Nolan', 148)
+    catalogue.add(movie2)
+
+    series1 = TVSeries('Scrubs', 2001, 'Bill Lawrence', 24, 9, 182)
+
     print(catalogue)
-except ValueError as e:
-    print(f"Validation Error: {e}")
