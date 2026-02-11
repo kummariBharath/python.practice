0553c9ede528ebbb1c2e1a2f062546c65013bcb2
kummariBharath <bharathkummari2611@gmail.com>
2026-02-11 11:48:53 +0530

    overrided the abstract methods

--- abstraction.py
+++ abstraction.py
@@ -3,7 +3,7 @@
 from abc import ABC,abstractmethod 
 class AbstractClass(ABC):
     @abstractmethod
-    def abstract_method(self):
+    def abstract_method(self): #@this method is abstract method and it must be implemented by the subclass
         pass
 
 # Concrete subclass that implements the abstract method
@@ -15,5 +15,23 @@ class ConcreteClassOne(AbstractClass):
 class ConcreteClassTwo(AbstractClass):
     def abstract_method(self):
         print('Implementation in ConcreteClassTwo')
-c=abstractmethod() #TypeError: Can't instantiate abstract class AbstractClass with abstract methods abstract_method
-c1=ConcreteClassOne() #
+#c=abstractmethod() #TypeError: Can't instantiate abstract class AbstractClass with abstract methods abstract_method
+c1=ConcreteClassOne() 
+c1.abstract_method()
+
+from abc import ABC,abstractmethod
+class Assignment(ABC):
+    def __init__(self,name):
+        self.name=name
+    @abstractmethod    
+    def submit(self):
+        pass
+class submit_assignment(Assignment):
+    def submit(self):
+        print(f"{self.name} submitted the assignment")
+class submit_project(Assignment):
+    def submit(self):
+        print(f"{self.name} submitted the project")        
+s=submit_assignment("bharath")
+print(s.name)
+print(s.submit())            
