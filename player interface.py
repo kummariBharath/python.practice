75d31dcf60cba258873515ffebef5dc1b9010359
kummariBharath <bharathkummari2611@gmail.com>
2026-02-13 23:42:53 +0530

    addded the required methods

--- player interface.py
+++ player interface.py
@@ -3,7 +3,7 @@ from abc import ABC,abstractmethod
 
 class Player(ABC):
     def __init__(self):
-        self.moves=[(0, 1), (0, -1), (1, 0), (-1, 0)]
+        self.moves=[]
         self.position=(0,0)
         self.path=[self.position]    
     def make_move(self):
@@ -15,3 +15,11 @@ class Player(ABC):
     @abstractmethod
     def level_up(self):
         pass
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def level_up(self):
+        self.moves.extend([(1, 1), (1, -1), (-1, 1), (-1, -1)])
