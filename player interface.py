30259d9199d6a03770d9e7109785cd84533c170d
kummariBharath <bharathkummari2611@gmail.com>
2026-02-13 23:29:52 +0530

    making changes

--- player interface.py
+++ player interface.py
@@ -1,12 +1,13 @@
+import random
 from abc import ABC,abstractmethod
-from shutil import move
+
 class Player(ABC):
     def __init__(self):
-        self.moves=[]
+        self.moves=[(0, 1), (0, -1), (1, 0), (-1, 0)]
         self.position=(0,0)
         self.path=[self.position]    
     def make_move(self):
-        random.choice(moves) 
+        move = random.choice(self.moves) 
         new_position=(self.position[0]+move[0],self.position[1]+move[1])
         self.path.append(new_position)
         self.position=new_position
