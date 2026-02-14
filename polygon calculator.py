0ae8f6c49795d5b32c950936594efb8b4cc1b210
kummariBharath <bharathkummari2611@gmail.com>
2026-02-14 22:30:49 +0530

    excuting with values

--- polygon calculator.py
+++ polygon calculator.py
@@ -1,6 +1,6 @@
 import math
 class Rectangle:
-    def __init__(self,uwidth,height):
+    def __init__(self,width,height):
         self.width=width
         self.height=height
     def set_width(self,width):
@@ -10,7 +10,6 @@ class Rectangle:
     def get_area(self):
         return self.width*self.height           
     def get_perimeter(self):
-       return 2(self.width*self.height)
         return 2 * (self.width + self.height)
     def get_diagonal(self):
         return math.sqrt(self.width**2+self.height**2)
@@ -36,11 +35,15 @@ class Square(Rectangle):
         self.height=side
     def set_height(self,side):
         self.height=side
-    def set_side(self,height,width):
         self.width=side
     def set_side(self,side):
         self.width=side
         self.height=side        
     def __str__(self):
-        return f"Square({side={self.side})"
         return f"Square(side={self.width})"
+    
+rec=Rectangle(10,5)
+print(rec.get_area())
+print(rec.get_perimeter())
+print(rec.get_diagonal()) #diagonal means the length of the line segment connecting two opposite corners of the rectangle
+
