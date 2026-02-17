e70c02b2f99e738c2df4f8dfc973e1570b464bf8
kummariBharath <bharathkummari2611@gmail.com>
2026-02-17 20:48:36 +0530

    Refactor employee profile generator to accept user input for personal details

--- empolyee profilr Generator.py
+++ empolyee profilr Generator.py
@@ -1,20 +1,18 @@
-first_name = 'John'
-last_name = 'Doe'
+first_name = input("Enter employee's first name: ")
+last_name = input("Enter employee's last name: ")
 full_name = first_name + ' ' + last_name
-address = '123 Main Street'
-address += ', Apartment 4B'
-employee_age = 28
+address = input("Enter employee's full address: ")
+employee_age = int(input("Enter employee's age: "))
 employee_info = full_name + ' is ' + str(employee_age)
 print(employee_info)
-years_experience = 5
-experience_years = 5
-experience_info = 'Experience: ' + str(experience_years) + ' years'
+years_experience = int(input("Enter years of experience: "))
+experience_info = 'Experience: ' + str(years_experience) + ' years'
 print(experience_info)
-position = 'Data Analyst'
-salary = 75000
+position = input("Enter employee's position: ")
+salary = int(input("Enter employee's salary: "))
 employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
 print(employee_card)
-employee_code = 'DEV-2026-JD-001'
+employee_code = input("Enter employee code (e.g., DEV-2026-JD-001): ")
 department = employee_code[0:3]
 print(department)
 year_code = employee_code[4:8]
