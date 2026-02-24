c916bf0a8665f15b9029d7ed528d6b51275df3a9
kummariBharath <bharathkummari2611@gmail.com>
2026-02-24 23:49:30 +0530

    Implement LinkedList class with Node inner class and basic methods

---
+++ linked list.py
@@ -0,0 +1,17 @@
+class LinkedList:
+    class Node:
+        def __init__(self, element):
+            self.element = element
+            self.next = None
+            
+    def __init__(self):
+        self.length = 0
+        self.head = None
+
+    def is_empty(self):
+        return self.length == 0
+    
+    def add(self, element):
+        node = self.Node(element)
+        if self.is_empty():
+            self.head = node
