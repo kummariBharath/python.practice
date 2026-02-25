72f4e8e0ab715f567f7f24d724ad272084bac7fe
kummariBharath <bharathkummari2611@gmail.com>
2026-02-25 12:50:31 +0530

    Add comment to clarify key-value storage in add method of HashTable class

--- hash table.py
+++ hash table.py
@@ -8,4 +8,4 @@ class HashTable:
 
     def add(self, key, value):
         hash_value = self.hash(key)
-        self.collection[hash_value] = {key: value}
+        self.collection[hash_value] = {key: value} # Store the key-value pair in the collection using the hash value as the key,key:value are values of the hash table
