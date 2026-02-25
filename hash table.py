65c5cfb7187ca715f58bc753dd4f2bf3441cc57e
kummariBharath <bharathkummari2611@gmail.com>
2026-02-25 16:41:18 +0530

    Refactor HashTable class to improve method structure and add missing newline at end of file

--- hash table.py
+++ hash table.py
@@ -9,3 +9,14 @@ class HashTable:
     def add(self, key, value):
         hash_value = self.hash(key)
         self.collection[hash_value] = {key: value} # Store the key-value pair in the collection using the hash value as the key,key:value are values of the hash table
+
+    def remove(self, key):
+        hash_value = self.hash(key)
+        if hash_value in self.collection and key in self.collection[hash_value]:
+            del self.collection[hash_value]
+
+    def lookup(self, key):
+        hash_value = self.hash(key)
+        if hash_value in self.collection and key in self.collection[hash_value]:
+            return self.collection[hash_value][key]
+        return None
