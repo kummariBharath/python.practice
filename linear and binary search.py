7bcee50b5c94d010b02723cccefeb8fa158647fd
kummariBharath <bharathkummari2611@gmail.com>
2026-03-07 23:18:39 +0530

    code push for binary

--- linear and binary search.py
+++ linear and binary search.py
@@ -9,3 +9,24 @@ def linear_search(arr, target):# arr is the list we want to search through, and 
 my_list = [5, 3, 2, 8, 1]
 result = linear_search(my_list, 8)
 print(f"Element found at index: {result}")
+
+#BINARY SEARCH
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2  
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+# Example usage:
+sorted_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = binary_search(sorted_list,5)
+print(f"Element found at index: {result}")
