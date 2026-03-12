544cb1421f675ac9c6e3838de56df4ec58dad534
kummariBharath <bharathkummari2611@gmail.com>
2026-03-12 11:45:47 +0530

    excuted

--- Data Structures.py
+++ Data Structures.py
@@ -1,6 +1,4 @@
 #binary search
-from ast import Raise
-
 
 def binary_search(search_list, value):
     path_to_target = []
@@ -24,52 +22,27 @@ print(binary_search([1, 2, 3, 4, 5], 3))
 print(binary_search([1, 2, 3, 4, 5, 9], 4))
 print(binary_search([1, 3, 5, 9, 14, 22], 10))
 
-#Merge sort
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-    sorted_list = []
-    i = 0
-    j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            sorted_list.append(left[i])
-            i += 1
-        else:
-            sorted_list.append(right[j])
-            j += 1
-
-    sorted_list.extend(left[i:])
-    sorted_list.extend(right[j:])
-
-    return sorted_list
-
 #bisection method
 def bisection_method(number, tol=1e-7, max_iter=1000):
     if number<0:
         raise ValueError('square of zero is not in real numbers')
     if number==0 or number==1:
         print(f"The squareroot of {number} is {number}")
+        return number
     if number<1:
         low=number
         high=1
-    if number>1:
+    else:
         low=1
         high=number
-    itertions=0
-    while (high-low)>tol and itertions<max_iter:
-        mid=low+high/2
+    iterations=0
+    while (high-low)>tol and iterations<max_iter:
+        mid=(low+high)/2
         if mid*mid>number:
             high=mid
         else:
             low=mid
-            iterations += 1
+        iterations += 1
 
     if iterations == max_iter:
         print(f"Failed to converge within {max_iter} iterations")
