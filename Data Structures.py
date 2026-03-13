9c7774f4b2d91578fee2c84669527e682857250d
kummariBharath <bharathkummari2611@gmail.com>
2026-03-13 18:46:35 +0530

    added luhn algo

--- Data Structures.py
+++ Data Structures.py
@@ -149,23 +149,3 @@ def selection_sort(items):
 
     return items 
     
-#luhn Algorithm 
-def verify_card_number(card_number):
-    card_number=card_number.replace(' ','').replace('-','')
-    digits=[int(d) for d in card_number][::-1]
-    total=0
-    for i in range(len(digits)):
-        num = digits[i]
-        if i%2==1:
-            num*=2
-            if num>9:
-                num-=9
-        total+=num        
-    if total % 10 == 0:
-        return "Valid!"
-    else:
-        return "Invalid"
-
-if __name__ == "__main__":
-    user_input = input("Enter a credit card number to verify: ")
-    print(verify_card_number(user_input))
