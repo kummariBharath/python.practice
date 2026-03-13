4dd870a49c723292c0851d4ecf2887df604da696
kummariBharath <bharathkummari2611@gmail.com>
2026-03-13 22:08:44 +0530

    ..

--- Shortest Path Algorithm.py
+++ Shortest Path Algorithm.py
@@ -1,9 +1,39 @@
 INF = float('inf')
 adj_matrix = [
-    [0, 3, INF, 7],
-    [8, 0, 2, INF],
-    [5, INF, 0, 1],
-    [2, INF, INF, 0]
+    [0, 5, 3, INF, 11, INF],
+    [5, 0, 1, INF, INF, 2],
+    [3, 1, 0, 1, 5, INF],
+    [INF, INF, 1, 0, 9, 3],
+    [11, INF, 5, 9, 0, INF],
+    [INF, 2, INF, 3, INF, 0],
 ]
+
 def shortest_path(matrix, start_node, target_node=None):
-    pass 
+    n = len(matrix)
+    distances = [INF] * n
+    distances[start_node] = 0
+    paths = [[node_no] for node_no in range(n)]
+    visited = [False] * n
+
+    for _ in range(n):
+        min_distance = INF
+        current = -1
+        for node_no in range(n):
+            if not visited[node_no] and distances[node_no] < min_distance:
+                min_distance = distances[node_no]
+                current = node_no
+    if target_node is not None :
+        targets=[target_node] 
+    else:
+        targets=range(n) 
+        if current == -1:
+            break
+        visited[current] = True
+
+        for node_no in range(n):
+            distance = matrix[current][node_no]
+            if distance != INF and not visited[node_no]:
+                new_distance = distances[current] + distance
+                if new_distance < distances[node_no]:
+                    distances[node_no] = new_distance
+                    paths[node_no] = paths[current] + [node_no]
