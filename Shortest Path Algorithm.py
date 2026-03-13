fc4451db00975c59edead42a7166d06efd2235fc
kummariBharath <bharathkummari2611@gmail.com>
2026-03-13 20:41:12 +0530

    added the definition

---
+++ Shortest Path Algorithm.py
@@ -0,0 +1,9 @@
+INF = float('inf')
+adj_matrix = [
+    [0, 3, INF, 7],
+    [8, 0, 2, INF],
+    [5, INF, 0, 1],
+    [2, INF, INF, 0]
+]
+def shortest_path(matrix, start_node, target_node=None):
+    pass
