9982b33bcea791cf8ceffd352729acef8d92dfb1
kummariBharath <bharathkummari2611@gmail.com>
2026-03-14 23:18:55 +0530

    converted into adjacency list

--- adjacency list to matrix converter.py
+++ adjacency list to matrix converter.py
@@ -1,4 +1,10 @@
 def adjacency_list_matrix_converter(adj_list):
     num_nodes=len(adj_list)
     matrix=[[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
-    
+    for node in adj_list:
+        for neighbour in adj_list[node]:
+            matrix[node][neighbour]=1
+
+    for row in matrix:
+        print(row)
+                
