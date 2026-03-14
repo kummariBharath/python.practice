bf693087ea395e3067bd2368b7c452ee6734ada8
kummariBharath <bharathkummari2611@gmail.com>
2026-03-14 23:38:27 +0530

    added logic with dictionary way

--- adjacency list to matrix converter.py
+++ adjacency list to matrix converter.py
@@ -1,9 +1,16 @@
 def adjacency_list_matrix_converter(adj_list):
-    num_nodes=len(adj_list)
+    # Find the maximum node index in the dictionary to get the correct matrix size
+    all_nodes = list(adj_list.keys()) # start with the keys of the dictionary, which are the nodes
+    for neighbors in adj_list.values():# loop through the values of the dictionary, which are lists of neighbors
+        all_nodes.extend(neighbors)
+        
+    num_nodes = max(all_nodes) + 1 if all_nodes else 0 # add 1 to get the correct size of the matrix, since node indices start at 0
     matrix=[[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
     for node in adj_list:
         for neighbour in adj_list[node]:
             matrix[node][neighbour]=1
+    for row in matrix:
+        print(row)        
     return matrix  
 A={
     0:[1,2],
