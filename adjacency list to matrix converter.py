a86c331f4febf1cd6b37cca8c43565159393c6d6
kummariBharath <bharathkummari2611@gmail.com>
2026-03-14 23:24:22 +0530

    adding inpput

--- adjacency list to matrix converter.py
+++ adjacency list to matrix converter.py
@@ -1,4 +1,10 @@
 def adjacency_list_matrix_converter(adj_list):
+    A={
+        0:[1,2],
+        1:[2],
+        2:[3,4],
+        3:[4,5]
+    }
     num_nodes=len(adj_list)
     matrix=[[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
     for node in adj_list:
@@ -7,4 +13,5 @@ def adjacency_list_matrix_converter(adj_list):
 
     for row in matrix:
         print(row)
-                
+    return matrix  
+A.adjacency_list_matrix_converter()  #call the function       
