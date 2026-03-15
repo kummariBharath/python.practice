26003070dfbed50224b2ef0dc2f7507140b96656
kummariBharath <bharathkummari2611@gmail.com>
2026-03-15 20:25:02 +0530

    BFS

---
+++ BFS.py
@@ -0,0 +1,11 @@
+def gen_parentheses(pairs):
+    if not isinstance(pairs, int):
+        return 'The number of pairs should be an integer'
+    if pairs < 1:
+        return 'The number of pairs should be at least 1'
+    queue = [('', 0, 0)]
+    result = []
+    while  queue!='': # while the queue is not empty
+        print(queue)
+    
+    return result
