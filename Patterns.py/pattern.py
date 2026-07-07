020861066770139505c6a33151fd058ce7b19ef4
kummariBharath <bharathkummari2611@gmail.com>
2026-07-07 22:46:10 +0530

    fix: complete pattern method implementation in pattern.py

--- Patterns.py/pattern.py
+++ Patterns.py/pattern.py
@@ -40,6 +40,10 @@ class Solution:
         for i in range(n):
             for j in range(n-i-2):
                 print(' ',end='')
-            for j in range    
+            for j in range(3*i+1):
+                print('$',end='')
+            for j in range(n-i-2):
+                print(' ',end='')
+            p           
 
                  
