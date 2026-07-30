6714bc216dc790bf336f1d5f740f3f8ee96d1b9f
kummariBharath <bharathkummari2611@gmail.com>
2026-07-30 23:11:31 +0530

    feat: add hollow square and triangle pattern implementations

--- Patterns.py/pattern.py
+++ Patterns.py/pattern.py
@@ -144,6 +144,27 @@ for i in range(1,n):
     else:
         print("prime number")
 
+#hollow square
+n=5
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end="")
+        else:
+            print(" ",end=" ")
+    print()
+
+#hollow triangle
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if j==1 or j==i or i==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()        
+        
+
 
 
     
