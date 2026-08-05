16b7bfc0752315685686fa3cc733e10c61cb30d3
kummariBharath <bharathkummari2611@gmail.com>
2026-08-05 14:14:04 +0530

    fix: clean up comments and formatting in Array.py for clarity

--- DSA/Array.py
+++ DSA/Array.py
@@ -29,11 +29,22 @@ print("modified array:",arr)
 #Array insertion using shift elements  to right
 arr=[10,20,30,40,0]
 n=4
-for i in range(n-1,-1,-1):
+for i in range(n-1,-1,-1): #start at i=4 ,i values are 3,2,1,0
     arr[i+1]=arr[i]
 arr[0]=50
 for i in range(n+1):
-    print(arr[i],end=' ')   
+    print(arr[i],end=' ')
+
+#array insertion of specific element using shift elemnets to right (custom )
+arr = [10,20,30,40,0] #array size is "5"
+n=4
+pos=2
+ele=5
+for i in range(n-1,pos-2,-1): # i range from 3 to 0 ,i values are 3,2,1 
+    arr[i+1]=arr[i]
+arr[pos-1]=ele
+
+       
 
 
 
