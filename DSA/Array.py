09e0115b916d4b94256f3a464ee41156b13a98f2
kummariBharath <bharathkummari2611@gmail.com>
2026-08-05 12:47:48 +0530

    feat: add searching functionality using traversal in Array.py

--- DSA/Array.py
+++ DSA/Array.py
@@ -7,5 +7,18 @@ for i in arr:
 print()
 
 #searching using traversal
+arr=[1,34,55,99]
+i=0
+target=55
+found=False
+for i in arr:
+    if arr[i]==target:
+        found=True
+        break
+if found:
+    print("found")
+else:
+    print("not found")        
+
 
 
