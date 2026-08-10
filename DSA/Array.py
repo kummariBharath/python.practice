2265eccabe0d1c18dabacc8f31e484a50589b441
kummariBharath <bharathkummari2611@gmail.com>
2026-08-10 11:37:23 +0530

    feat: remove unused import and clean up whitespace in Array.py

--- DSA/Array.py
+++ DSA/Array.py
@@ -47,9 +47,6 @@ arr[pos-1]=ele
 print(arr)
 
 """
-from turtle import left
-
-
 #deletion of specific element using shift elements to left
 arr=[10,20,30,40]
 n=4
@@ -59,6 +56,13 @@ for i in range(pos,n-1):
 n-=1
 for i in range(n):
     print(arr[i],end=' ')
+
+ #deletion of first occurrence of specific element using remove()
+arr=list(map(int,input().split()))
+
+
+ 
+
 
    
 
