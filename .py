7e17003b2c9c218a8e0f70bca5488a1a4b359db5
kummariBharath <bharathkummari2611@gmail.com>
2026-08-11 23:05:55 +0530

    fix: return 0 for base case in sum function to prevent None return

--- .py
+++ .py
@@ -297,6 +297,6 @@ printNumbers(5)
 
 def sum(n):
     if n<=0:
-        return 
-    return n+sum(n-1)
-print(sum(5))                
+        return 0
+    return n+sum(n-1)     
+print(sum(5))        
